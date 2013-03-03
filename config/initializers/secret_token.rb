8f1618dcd7a7206be346d1f4dbef3a98fbd16492
Icee Griffin <igriffin29@gmail.com>
2013-03-03 08:56:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miga::Application.config.secret_token = '2e8d9d47cb44991371267a1209196189e1ad4c7e5405dc805a357867590fe61864dc227979d01070d8d71e1834f72e33b0b489c6137c2cf5d815113f7f8ccdd6'
